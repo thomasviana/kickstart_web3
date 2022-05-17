5030bd52a602b3bd3145d8b12dddf9504935dbbc
Thomas Viana <thomasviana16@gmail.com>
2022-05-17 15:24:55 -0500

    New factory instance.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import web3 from './web3';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x0180CB880451d085b46004740B03386c2d33Dfe2'
+  '0xA4862b796876d7b3Ae0ea89A2f8FBcf6d41a2239'
 );
 
 export default instance;
