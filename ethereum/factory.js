b5d89a3f89ee3947223f9a183c9ebb2025da4879
Thomas Viana <thomasviana16@gmail.com>
2022-05-16 23:26:45 -0500

    Pass new address on factory contract instance.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import web3 from './web3';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x39aeDe7256C61274cB30bd1413C138212Dc12dfC'
+  '0x0180CB880451d085b46004740B03386c2d33Dfe2'
 );
 
 export default instance;
