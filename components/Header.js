4d196ae63934e0797d0cce7fab326fba5b068b8c
Thomas Viana <thomasviana16@gmail.com>
2022-05-16 23:25:46 -0500

    Update header component.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
-<<<<<<< HEAD
 import { Link } from '../routes';
-=======
->>>>>>> 1dc460f484dad357095fb753a3d82dfa4a3fd3b1
 
 const Header = (props) => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-<<<<<<< HEAD
       <Link route="/">
         <a className="item">K i c k S t a r t</a>
       </Link>
@@ -20,14 +16,6 @@ const Header = (props) => {
         <Link route="/campaigns/new">
           <a className="item">+</a>
         </Link>
-=======
-      <Menu.Item>Kickstart</Menu.Item>
-
-      <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
-
-        <Menu.Item>+</Menu.Item>
->>>>>>> 1dc460f484dad357095fb753a3d82dfa4a3fd3b1
       </Menu.Menu>
     </Menu>
   );
