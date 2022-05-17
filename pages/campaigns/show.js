612b8246ec26d17c3f62e64df0a776f7f1f89fea
Thomas Viana <thomasviana16@gmail.com>
2022-05-16 23:27:24 -0500

    Get address from wildcard query to show contract details con show.js.

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-
+import Layout from '../../components/Layout';
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    console.log(props.query.address);
+    return {};
+  }
+
   render() {
-    return <div>Campaign Show</div>;
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+      </Layout>
+    );
   }
 }
 
